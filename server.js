fa37f4aa5c35aea2b53f6ec7d1b4aa000419cba2
SpencerTower <spencerjtower1@gmail.com>
2022-04-22 16:55:31 -0700

    adds GET route to get tourby id

--- server.js
+++ server.js
@@ -26,9 +26,27 @@ const tours = JSON.parse(
 // add new tour to route
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body);
-  res.json('Post route hit.')
-})
+  // get last tour, get its id, add 1 to create new tour id
+  const newId = tours[tours.length - 1].id + 1;
+  // combine id with new tour object
+  const newTour = Object.assign({ id: newId }, req.body);
 
+  console.log('TOURS BEFORE PUSH', tours);
+  tours.push(newTour);
+  console.log('TOURS AFTER PUSH', tours); // add new tour object to tours array
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour
+        }
+      })
+    })
+});
+
+// Get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'Success',
@@ -37,6 +55,27 @@ app.get('/api/v1/tours', (req, res) => {
     }
   })
 });
+
+// Get one tour
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  // set id to req id and multiply by 1 to turn its type from string to number
+  const id = req.params.id * 1;
+
+  // check if matching id in tours
+  const tour = tours.find(elem => elem.id === id);
+  if (tour) {
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        tour: tour,
+      }
+    })
+  } else {
+    res.status(404).json('Tour not found.');
+  }
+});
+
 
 
 const port = 3000;
