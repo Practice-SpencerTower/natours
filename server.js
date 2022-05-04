74b2044ebd1a46067feaa330626cbed3272408b2
SpencerTower <spencerjtower1@gmail.com>
2022-05-04 16:45:47 -0700

    adds mongoose dependency, connects mongoose to db in server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>', process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => console.log('DB Connection Successful.'));
 
 console.log(app.get('env')); // returns environment node app is runing in
 // console.log(process.env);
