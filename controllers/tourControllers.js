222d6c6adb4e577f1d7492904425b780eda2a4d8
SpencerTower <spencerjtower1@gmail.com>
2022-05-17 12:03:49 -0700

    adds filter, sort, limitFields, and paginate methods to APIFilter class to allow reuse of methods across handlers

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -24,12 +24,36 @@ class APIFeatures {
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); // find parameter in query and add $ to convert to mongoose query
 
-        this.query.find(JSON.parse(queryStr));
+        this.query = this.query.find(JSON.parse(queryStr));
         // let query = Tour.find(JSON.parse(queryStr));
+        return this;
     }
 
     sort() {
-        
+        if (this.query.sort) {
+            const sortBy = this.query.sort.split(',').join(' '); // replace comma with space in sort query
+            query = this.query.sort(sortBy);
+        } else {
+            query = thisquery.sort('-createdAt');
+        }
+        return this;
+    }
+
+    limitFields() {
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(',').join(' '); // parse fields from query
+            this.query = this.query.select(fields); // projecting = selecting specific field names
+        } else {
+            this.query = this.query.select('-__v');
+        }
+        return this;
+    }
+
+    paginate() {
+        const page = req.queryString.page * 1 || 1;  // define first page by default
+        const limit = req.queryString.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit); // need to skip a certain number of results to get to a specific page
     }
 }
 
@@ -44,54 +68,14 @@ exports.getAllTours = async (req, res) => {
     console.log('Get All Tours Route Hit.');
 
     try {
-        // BUILD QUERY
-        // 1) Filtering
-        // const queryObj = {...req.query};
-        // const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        // excludedFields.forEach(el => delete queryObj[el]); // delete excluded fields
 
-
-        // // 2) Advanced filtering
-        // let queryStr = JSON.stringify(queryObj);
-        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        // console.log(JSON.parse(queryStr));
-
-
-        // let query = Tour.find(JSON.parse(queryStr));
-
-
-        console.log('REQ.QUERY', req.query);
-        console.log('REQ QUERY TYPE: ', typeof req.query);
-        console.log('REQ QUERY SORT: ', req.query.sort);
-        // 2) Sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' '); // replace comma with space in sort query
-            query = query.sort(req.query.sort);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        // 3) Field limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' '); // parse fields from query
-            query = query.select(fields); // projecting = selecting specific field names
-        } else {
-            query = query.select('-__v');
-        }
-
-        // 4) Pagination
-        const page = req.query.page * 1 || 1;  // define first page by default
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-        // page=2&limit=10, 1-10, page 1, 11-20, page 2, 21-30, page 3
-        query = query.skip(skip).limit(limit); // need to skip a certain number of results to get to a specific page
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) throw new Error('This page does not exist.');  // throw new error will automatically trigger catch block
-        }
         // EXECUTE QUERY
-        const features = new APIFeatures(Tour.find(), req.query).filter();
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        
         const tours = await features.query;
 
         // SEND RESPONSE
