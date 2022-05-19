55894fed05f450d7d71229996668f6ea1d7a8f45
SpencerTower <spencerjtower1@gmail.com>
2022-05-19 15:21:03 -0700

    adds slugify dependency, adds tourSchema pre middleware that adds slug to new tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true,
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration'],
@@ -59,6 +61,23 @@ const tourSchema = new mongoose.Schema({
 
 tourSchema.virtual('durationWeeks').get(function() {
     return this.durations / 7; // convert days to weeks
+});
+
+// in save middleware, 'this' points to document that is being saved
+tourSchema.pre('save', function(next) {
+    console.log('THIS', this);
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+tourSchema.pre('save', function(next) {
+    console.log('Will save document...');
+    next();
+});
+
+tourSchema.post('save', function(doc, next) {
+    console.log('DOC', doc);
+    next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
